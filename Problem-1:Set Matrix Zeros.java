43cb8073ca44a9245e55bb92a2325954f955e0fa
Rishav Dutta <92175535+Pullu05@users.noreply.github.com>
2022-06-05 00:58:33 +0530

    Update Problem-1:Set Matrix Zeros.java

--- Problem-1:Set Matrix Zeros.java
+++ Problem-1:Set Matrix Zeros.java
@@ -2,25 +2,19 @@ import java.util.* ;
 import java.io.*; 
 public class Solution {
     public static void setZeros(int matrix[][]) {
-        // Write your code here..
         ArrayList<Integer> a = new ArrayList<>();
-        ArrayList<Integer> a1 = new ArrayList<>();
         for(int i=0;i<matrix.length;i++){
+            int pos=-1;
             for(int j=0;j<matrix[i].length;j++){
                if(matrix[i][j]==0){
-                    a.add(j);
-                   a1.add(i);
+                   pos= i;
+                   a.add(j);
                 }
+             }
+            if(pos>=0){
+                Arrays.fill(matrix[pos],0);
             }
-         }
-        //System.out.println("col:"+a);
-        //System.out.println("row:"+a1);
-        
-         if(a1.size()>0){
-                for(int i=0;i<a1.size();i++){
-                      Arrays.fill(matrix[a1.get(i)],0);
-                } 
-            }
+        }
         
          if(a.size()>0){
                 for(int i=0;i<matrix.length;i++){
@@ -28,5 +22,5 @@ public class Solution {
                     matrix[i][a.get(j)]=0;
                 } 
             }
-   }
+         }
 }    
